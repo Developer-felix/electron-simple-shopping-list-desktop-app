3f32a4017032995042e49807d139f709951a1f81
Developer-felix <onjombafelix1@gmail.com>
2021-02-25 10:17:16 -0800

    wrapping up my hands after coding django rest framework now i make change to my new application

---
+++ videoinfo.js
@@ -0,0 +1,7 @@
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+
+app.on("ready", () => {
+    const mainWindow = new BrowserWindow({})
+    mainWindow.loadURL(`file://{__dirname}\videoinfo.html`);
+});
